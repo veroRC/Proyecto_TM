b6e651c184da809adb3ee10c214b69cdd68205ea
jose-z-m <65821332+jose-z-m@users.noreply.github.com>
2020-06-14 23:18:03 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/proyecto_tm/TerceraActivity.java
@@ -0,0 +1,63 @@
+package com.example.proyecto_tm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class TerceraActivity extends AppCompatActivity {
+
+    protected static final int TIMER_RUNTIME = 10000;
+    protected boolean nbActivo;
+    protected ProgressBar nProgressBar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terceraactividad);
+
+        nProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        final Thread timerThread = new Thread() {
+            @Override
+            public void run() {
+                nbActivo = true;
+                try {
+                    int espera = 0;
+                    while (nbActivo && (espera < TIMER_RUNTIME)) {
+                        sleep(200);
+                        if (nbActivo) {
+                            espera += 200;
+                            actualizarProgress(espera);
+                        }
+                    }
+                } catch (InterruptedException e) {
+                } finally {
+                    onContinuar();
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    public void actualizarProgress(final int timePassed){
+        if(null != nProgressBar){
+            final int progress = nProgressBar.getMax() * timePassed
+                    /TIMER_RUNTIME;
+            nProgressBar.setProgress(progress);
+        }
+    }
+    public void onContinuar(){
+        Log.d("mensajeFinal", "Su barra de progreso acaba de cargar");
+    }
+
+
+    public void onClickIniciarSesion(View view){
+        Intent Sesion = new Intent(this,CuartaActivity.class);
+        startActivity(Sesion);
+    }
+
+}
